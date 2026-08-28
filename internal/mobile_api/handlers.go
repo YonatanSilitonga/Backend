52cf23bcb3791595f22fb8000677fc4d4484ed41
WhisnuAbraham <whisnu.abraham.l.s@gmail.com>
2026-08-28 11:53:46 +0700

    update version code for mobile

--- internal/mobile_api/handlers.go
+++ internal/mobile_api/handlers.go
@@ -47,11 +47,11 @@ type AppVersionResponse struct {
 
 func (h *APIHandler) GetAppVersion(c echo.Context) error {
 	return c.JSON(http.StatusOK, AppVersionResponse{
-		VersionCode:  4,
-		VersionName:  "1.0.3",
+		VersionCode:  5,
+		VersionName:  "1.0.4",
 		DownloadURL:  "https://api.controltowerslb.tech/uploads/apk/tower-control-latest.apk",
 		ForceUpdate:  false,
-		ReleaseNotes: "Notifikasi update otomatis",
+		ReleaseNotes: "Pembaruan tampilan beranda & penyempurnaan riwayat ritase.",
 	})
 }
 
